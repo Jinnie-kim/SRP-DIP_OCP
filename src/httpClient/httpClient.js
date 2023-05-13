c42873757b44faf2bcc10f046873a143e057e8f1
Jinnie <hjk7639@gmail.com>
2023-05-13 18:06:03 +0900

    HttpClient 모듈 클래스 분리

---
+++ src/httpClient/httpClient.js
@@ -0,0 +1,20 @@
+// 추상
+// fetch(endPoint, options): Promise
+
+export class HttpClient {
+  constructor(baseURL, tokenRepository) {
+    this.baseURL = baseURL;
+    this.tokenRepository = tokenRepository;
+  }
+
+  fetch(url, options = {}) {
+    return window.fetch(`${this.baseURL}${url}`, {
+      ...options,
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: this.tokenRepository.get(),
+        ...options.headers,
+      },
+    });
+  }
+}
