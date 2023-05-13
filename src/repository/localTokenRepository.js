844bec63ad68881e945a4325a42ed19144a3546d
Jinnie <hjk7639@gmail.com>
2023-05-13 18:06:40 +0900

    token 저장 & 관리 모듈 분리

---
+++ src/repository/localTokenRepository.js
@@ -0,0 +1,24 @@
+// 추상
+/*
+	TokenRepositoryInterface
+    save(token:string):void
+    get():string
+    delete():void
+*/
+
+// 구체
+export class LocalTokenRepository {
+  #TOKEN_KEY = 'ACCESS_TOKEN';
+
+  save(token) {
+    localStorage.setItem(this.#TOKEN_KEY, token);
+  }
+
+  get() {
+    return localStorage.getItem(this.#TOKEN_KEY);
+  }
+
+  delete() {
+    localStorage.removeItem(this.#TOKEN_KEY);
+  }
+}
