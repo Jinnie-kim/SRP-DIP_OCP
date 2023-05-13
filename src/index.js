0a2412cb8449f3d7ed1d3c27d0022e5142953a61
Jinnie <hjk7639@gmail.com>
2023-05-13 20:26:03 +0900

    AuthProvider적용, authService를 value 값으로 공유

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { LocalTokenRepository } from './repository/localTokenRepository';
+import { HttpClient } from './httpClient/httpClient';
+import { AuthService } from './service/AuthService';
+import App from './App';
+import { AuthProvider } from './context/AuthProvider';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+// index.js
+// bootstraping
+
+// make tokenRepository
+// make httpClient
+// make authService
+
+const localTokenRepository = new LocalTokenRepository();
+const httpClient = new HttpClient('https://www.pre-onboarding-selection-task.shop/', localTokenRepository);
+const authServie = new AuthService(httpClient, localTokenRepository);
+
+root.render(
+  <AuthProvider authService={authServie}>
+    <App />
+  </AuthProvider>
+);
